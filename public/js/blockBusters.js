64ef592a67c6036489cf0da4a0a83d22de5392aa
rcurry7 <rcurry87@gmail.com>
2022-07-25 11:32:11 +0100

    test

---
+++ public/js/blockBusters.js
@@ -0,0 +1,17 @@
+
+
+for (i=2; i < 101; i++) {
+console.log("test")
+ const newDiv = document.createElement("div");
+ newDiv.setAttribute('id', "block-" + i);
+ newDiv.setAttribute('class', "red-block");
+ document.getElementById("blocks").appendChild(newDiv);
+
+
+ 
+}
+
+
+
+object.addEventListener("mouseover", function ());
+ 
